fb8b2ca6017a8195c02371057e10a3fefc59f198
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-08-14 13:18:26 +0530

    Parent-Child

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
-const heading = React.createElement("h1",{},"Hello World by react ");
+const heading = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [
+            React.createElement("h1",{},"H1 tag 1"),
+            React.createElement("h1",{},"H1 tag 2")
+        ]
+    ));
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
     root.render(heading);
